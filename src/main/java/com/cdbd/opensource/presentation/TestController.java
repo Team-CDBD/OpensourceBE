1fdcc26fc5654ecb39fc28b1d8523fc370dbdf38
memojun-admin <junmozzang97@gmail.com>
2025-07-09 10:30:59 +0900

    add : swagger 기본설정 적용

--- src/main/java/com/cdbd/opensource/presentation/TestController.java
+++ src/main/java/com/cdbd/opensource/presentation/TestController.java
@@ -1,16 +1,27 @@
 package com.cdbd.opensource.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 레이어드 아키텍쳐를 위한 테스트 코드로 추후 내용 작성되면 삭제 해주세요.
  */
+@Tag(name = "test", description = "스웨거 test")
 @RestController
+@RequestMapping("/api/v1")
 public class TestController {
 	
 	@GetMapping("/")
 	public String test() {
 		return "테스트";
+	}
+
+	@Operation(summary = "test", description = "Operation 테스트")
+	@GetMapping("/posts")
+	public String getPosts() {
+		return "succ";
 	}
 }

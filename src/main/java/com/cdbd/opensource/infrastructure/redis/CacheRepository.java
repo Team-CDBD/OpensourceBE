3d2c764754b41bf0ff9c1533ddba1f035cd1c218
shAn-kor <kjn0406@naver.com>
2025-08-07 23:42:32 +0900

    Feat : Add Cache TTL Default

--- src/main/java/com/cdbd/opensource/infrastructure/redis/CacheRepository.java
+++ src/main/java/com/cdbd/opensource/infrastructure/redis/CacheRepository.java
@@ -1,7 +1,6 @@
 package com.cdbd.opensource.infrastructure.redis;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
@@ -11,13 +10,13 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class CacheRepository {
-    private static final Duration TTL = Duration.ofMinutes(10);
+    private static final Duration TTL = Duration.ofDays(30);
 
-    private final RedisTemplate<String, Object> redisTemplate;
     private final ValueOperations<String, Object> valueOps;
 
     public void save(RedisRequest request) {
         String key = generateKey(request);
+        Duration ttl = Optional.ofNullable(request.ttl()).orElse(TTL);
         RedisResponse response = RedisResponse.builder()
                 .className(request.className())
                 .method(request.method())
@@ -25,7 +24,7 @@ public class CacheRepository {
                 .message(request.message())
                 .severity(request.severity())
                 .futureCalls(request.futureCalls())
-                .ttl(TTL)
+                .ttl(ttl)
                 .build();
         valueOps.set(key, response, TTL);
     }
@@ -38,6 +37,12 @@ public class CacheRepository {
             return Optional.of(cachedResponse);
         }
         return Optional.empty();
+    }
+
+    public void delete(RedisRequest request) {
+        String key = generateKey(request);
+
+        valueOps.getOperations().delete(key);
     }
 
     private String generateKey(RedisRequest request) {
--- src/main/java/com/cdbd/opensource/infrastructure/redis/RedisRequest.java
+++ src/main/java/com/cdbd/opensource/infrastructure/redis/RedisRequest.java
@@ -1,8 +1,8 @@
 package com.cdbd.opensource.infrastructure.redis;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+import java.time.Duration;
 import java.util.List;
 
 @Builder
@@ -12,6 +12,7 @@ public record RedisRequest(
         int line,
         String message,
         String severity,
-        List<String> futureCalls
+        List<String> futureCalls,
+        Duration ttl
 ) {
 }

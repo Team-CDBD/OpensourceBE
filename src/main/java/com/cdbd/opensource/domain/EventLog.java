35b5f5afa142fbf5a2818186d2bcf621c1c1ef41
shAn-kor <kjn0406@naver.com>
2025-08-15 01:58:01 +0900

    Fix : Delete Setter, Builder

--- src/main/java/com/cdbd/opensource/domain/EventLog.java
+++ src/main/java/com/cdbd/opensource/domain/EventLog.java
@@ -1,15 +1,11 @@
 package com.cdbd.opensource.domain;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
 
 @Getter
-@Setter
-@Builder
 @ToString
 public class EventLog {
     private String className;

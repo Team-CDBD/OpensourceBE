fe1389972ee8a5246ca9aaeb179e7e3b08975a4d
DESKTOP-5JPIU0I\USER <repaid11010@naver.com>
2025-08-20 01:27:47 +0900

    add : 계정 서비스 및 controller 추가

--- src/main/java/com/cdbd/opensource/presentation/AuthController.java
+++ src/main/java/com/cdbd/opensource/presentation/AuthController.java
@@ -1,26 +1,27 @@
 package com.cdbd.opensource.presentation;
 
+import com.cdbd.opensource.application.account.AccountService;
 import com.cdbd.opensource.application.config.security.JwtTokenProvider;
 import com.cdbd.opensource.presentation.dto.login.LoginRequest;
 import com.cdbd.opensource.presentation.dto.login.TokenResponse;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
     private final JwtTokenProvider jwtProvider;
-
-    public AuthController(JwtTokenProvider jwtProvider) {
-        this.jwtProvider = jwtProvider;
-    }
+    private final AccountService service;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest req) {
-        // 여기서는 그냥 하드코딩된 사용자 검증
-        if ("test".equals(req.getUsername()) && "1234".equals(req.getPassword())) {
+        if (this.service.login(req.getUsername(), req.getPassword())) {
             String token = jwtProvider.createToken(req.getUsername());
             return ResponseEntity.ok(new TokenResponse(token));
         }

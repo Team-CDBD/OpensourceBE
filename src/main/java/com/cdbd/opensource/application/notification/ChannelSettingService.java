5eaf05f89656f7c8f8732d92616999239a936a13
Hee <ycross4008@gmail.com>
2025-08-19 23:27:31 +0900

    add : 채널 세팅 리스트 조회 및 저장 후 로드 수정

--- src/main/java/com/cdbd/opensource/application/notification/ChannelSettingService.java
+++ src/main/java/com/cdbd/opensource/application/notification/ChannelSettingService.java
@@ -6,6 +6,8 @@ import com.cdbd.opensource.infrastructure.notification.ChannelSettingManager;
 import com.cdbd.opensource.infrastructure.notification.entity.ChannelSetting;
 import com.cdbd.opensource.infrastructure.notification.repository.ChannelSettingRepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +17,9 @@ public class ChannelSettingService {
 	private final ChannelSettingRepository repository;
 	private final ChannelSettingManager manager;
 	
+	@PersistenceContext
+	private final EntityManager em;
+	
 	@Transactional
 	public ChannelSetting create(String botName, String channelType, String settings) {
 		// 1. 채널 세팅 저장
@@ -23,6 +28,10 @@ public class ChannelSettingService {
 				.channelType(channelType)
 				.settings(settings)
 				.build();
+		
+		em.persist(channelSetting);
+		
+		em.flush();
 		
 		// 2. 채널 세팅 reload
 		manager.reload();
--- src/main/java/com/cdbd/opensource/presentation/notification/ChannelSettingController.java
+++ src/main/java/com/cdbd/opensource/presentation/notification/ChannelSettingController.java
@@ -1,15 +1,18 @@
 package com.cdbd.opensource.presentation.notification;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdbd.opensource.application.notification.ChannelSettingService;
+import com.cdbd.opensource.infrastructure.notification.ChannelSettingManager;
 import com.cdbd.opensource.infrastructure.notification.entity.ChannelSetting;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,7 +23,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChannelSettingController {
 	private final ChannelSettingService channelSettingService;
+	private final ChannelSettingManager channelSettingManager;
 	private final ObjectMapper objectMapper;
+	
+	@GetMapping
+    public ResponseEntity<List<ChannelSetting>> listAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(channelSettingManager.getAll());
+    }
 	
 	@PostMapping
 	public ResponseEntity<ChannelSetting> create(@RequestBody Map<String, Object> body) {
@@ -37,6 +46,4 @@ public class ChannelSettingController {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedChannelSetting);
 	}
-	
-	
 }

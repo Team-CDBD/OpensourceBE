3af73a7b8cfddd8b4c0b4367e84e0d4918fa1c36
shAn-kor <kjn0406@naver.com>
2025-08-21 00:30:24 +0900

    Fix : Add Getter To Jackson

--- src/main/java/com/cdbd/opensource/presentation/eventlog/PageResponseDto.java
+++ src/main/java/com/cdbd/opensource/presentation/eventlog/PageResponseDto.java
@@ -1,10 +1,14 @@
 package com.cdbd.opensource.presentation.eventlog;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Builder
+@Getter
+@ToString
 public class PageResponseDto<T> {
     private List<T> content;
     private int currentPage;
